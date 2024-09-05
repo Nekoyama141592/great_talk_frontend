f07c0a272feb29a6644d6e0373a20096e97a5703
Nekoyama141592 <mtaky0904@gmail.com>
2024-09-05 21:26:28 +0900

    change: AppでUserをfetchする

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,30 @@
 import './App.css'
 import First from './screen/first';
+import { auth } from "../infrastructures/firebase"
+import { useDispatch } from "react-redux"
+import { initUser,clearUser } from "../reducers"
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect} from 'react'
 
-
-function App() { 
+function App() {
+  const dispatch = useDispatch();
+  const init = () => {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            const {email,uid} = user;
+            const payload = {email,uid}
+            const action = initUser(payload);
+            dispatch(action);
+        } else {
+            const payload = {};
+            const action = clearUser(payload);
+            dispatch(action);
+        }
+      });
+  }
+  useEffect(() => {
+    init();
+  },[]);
   return (
     <>
       <First></First>
--- src/pages/screen/first/index.tsx
+++ src/pages/screen/first/index.tsx
@@ -1,33 +1,11 @@
 import Auth from '../auth';
 import Home from '../home';
 import Loading from '../loading';
-import { auth } from "../../../infrastructures/firebase"
-import { useDispatch,useSelector } from "react-redux"
-import { initUser,clearUser } from "../../../reducers"
-import { onAuthStateChanged } from 'firebase/auth';
-import { useEffect} from 'react'
+import { useSelector } from "react-redux"
 function First() { 
   const firstLoaded = useSelector((state: any) => state.authReducer.firstLoaded);
   const email = useSelector((state: any) => state.authReducer.email);
-  const dispatch = useDispatch();
   
-  const init = () => {
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            const {email,uid} = user;
-            const payload = {email,uid}
-            const action = initUser(payload);
-            dispatch(action);
-        } else {
-            const payload = {};
-            const action = clearUser(payload);
-            dispatch(action);
-        }
-      });
-  }
-  useEffect(() => {
-    init();
-  },[]);
   if (!firstLoaded) {
     return <Loading></Loading>
   }
