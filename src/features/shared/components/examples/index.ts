ae10eaa9a8e119f132f2d369948c217d28bf790d
Nekoyama141592 <mtaky0904@gmail.com>
2025-07-13 17:22:56 +0900

    remove: 不要な記述

--- src/features/shared/components/examples/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * DIKW Architecture Examples
- * Export all example components demonstrating DIKW integration
- */
-
-export { PostsFeedExample } from './PostsFeedExample'
-export { AdminDashboardExample } from './AdminDashboardExample'
